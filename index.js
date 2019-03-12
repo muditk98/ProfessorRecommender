830b38ff0fff3031de8724f5b7be6b8d468cc235
TheWussPuss <kapsmudit@gmail.com>
2019-03-12 20:18:34 +0530

    k

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.urlencoded({
 	extended: true
 }));
 app.use(express.json());
-app.use(express.static('./css'));
+app.use(express.static('./public'));
 app.set('views', './views');
 app.set('view engine', 'pug');
 
